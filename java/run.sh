0ebe3523e8dabc70b65fb7819863c4c17066b435
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-03-14 20:37:40 +0900

    minimum runtime for java, c

---
+++ java/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# イメージ名とタグを設定
+IMAGE_NAME="hello-java"
+TAG="1.0"
+
+# Dockerイメージをビルド
+echo "Building Docker image..."
+docker build -t ${IMAGE_NAME}:${TAG} .
+
+# コンテナを実行
+echo "Running container..."
+docker run --rm ${IMAGE_NAME}:${TAG}
+
+# 不要になったイメージを削除（オプション）
+read -p "Do you want to remove the Docker image? (y/N): " answer
+if [[ $answer =~ ^[Yy]$ ]]; then
+    echo "Removing Docker image..."
+    docker rmi ${IMAGE_NAME}:${TAG}
+fi
