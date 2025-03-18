0b51280c85a39e1ba84216fd37342be839b6889e
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-03-18 22:56:10 +0900

    4.2.1 トークン型

---
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,51 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+    // Single-character tokens.
+    COMMA,
+    DOT,
+    LEFT_BRACE,
+    LEFT_PAREN,
+    MINUS,
+    PLUS,
+    RIGHT_BRACE,
+    RIGHT_PAREN,
+    SEMICOLON,
+    SLASH,
+    STAR,
+    
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    
+    // Literals.
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+    
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FOR,
+    FUN,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+    
+    EOF
+}
