420b3f25b4aef03401a7551f63dfdde24e89b9e4
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-03-29 22:09:28 +0900

    refactoring

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -7,6 +7,11 @@ class Interpreter imprements Expr.Visitor<Object> {
     }
 
     @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
@@ -67,11 +72,6 @@ class Interpreter imprements Expr.Visitor<Object> {
         if (object == null) return false;
         if (object instanceof Boolean) return (boolean)object;
         return true;
-    }
-
-    @Override
-    public Object visitGroupingExpr(Expr.Grouping expr) {
-        return evaluate(expr.expression);
     }
 
     private boolean isEqual(Object a, Object b) {
