98f609383c7400cb490419e1790690270d900c64
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-03-29 21:40:10 +0900

    7.2.3 単項式を評価する

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -7,6 +7,21 @@ class Interpreter imprements Expr.Visitor<Object> {
     }
 
     @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return -(double)right;
+            case BANG:
+                return !isTruthy(right);
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
