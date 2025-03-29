d7e2334032f46c504918fa2289c6657fb9dd2adc
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-03-29 21:39:05 +0900

    7.2.2 丸カッコのペアを評価する

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -5,4 +5,14 @@ class Interpreter imprements Expr.Visitor<Object> {
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
     }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    // 渡された Expr クラスに評価を委譲する
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
 }
