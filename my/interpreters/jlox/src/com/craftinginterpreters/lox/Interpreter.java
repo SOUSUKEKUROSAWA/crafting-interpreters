9eebf8a7995d1e7582763c42763303aeda65dcd7
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-03-29 21:36:56 +0900

    7.2.1 リテラルを評価する

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,4 +1,8 @@
 package com.craftinginterpreters.lox;
 
 class Interpreter imprements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
 }
