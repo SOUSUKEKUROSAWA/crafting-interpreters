bd9a53c3b75ed5607082ef7d6a6212e32ece0d44
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-03-29 13:31:55 +0900

    refactoring

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Lox.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -59,18 +59,18 @@ public class Lox {
         report(line, "", message);
     }
 
-    private static void report(int line, String where, String message) {
-        System.err.println(
-            "[line " + line + "] Error" + where + ": " + message
-        );
-        hadError = true; // エラーが報告されたタイミングでフラグを立てる
-    }
-
     static void error(Token token, String message) {
         if (token.type == TokenType.EOF) {
             report(token.line, " at end", message);
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message
+        );
+        hadError = true; // エラーが報告されたタイミングでフラグを立てる
     }
 }
