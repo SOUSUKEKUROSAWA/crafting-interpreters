933d32831278c610d8507109a1c813b255dd86c8
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-04-02 23:40:04 +0900

    7.3.1 ランタイムエラーを検出する

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -17,6 +17,7 @@ class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case MINUS:
+                checkNumberOperands(expr.operator, right);
                 return -(double)right;
             case BANG:
                 return !isTruthy(right);
@@ -33,10 +34,13 @@ class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left - (double)right;
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left / (double)right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left * (double)right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
@@ -47,14 +51,20 @@ class Interpreter implements Expr.Visitor<Object> {
                     return (String)left + (String)right;
                 }
 
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+
                 break;
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double)right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double)right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double)right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double)right;
             case BANG_EQUAL:
                 return !isEqual(left, right);
@@ -84,5 +94,15 @@ class Interpreter implements Expr.Visitor<Object> {
     // 渡された Expr クラスに評価を委譲する
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void checkNumberOperands(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 }
