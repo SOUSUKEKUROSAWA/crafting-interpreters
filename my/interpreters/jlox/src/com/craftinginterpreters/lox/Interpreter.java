c3be366f7740957a79c946902b407cf1221dcb6c
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-04-06 00:04:10 +0900

    0による除算でエラーを吐くように修正

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -47,6 +47,9 @@ class Interpreter implements Expr.Visitor<Object> {
                 return (double)left - (double)right;
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
+                if ((double)right == 0) {
+                    throw new RuntimeError(expr.operator, "Division by zero.");
+                }
                 return (double)left / (double)right;
             case STAR:
                 checkNumberOperands(expr.operator, left, right);
