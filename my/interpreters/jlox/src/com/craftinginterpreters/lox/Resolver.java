b1d1eac7ff0a24b36808ab9db02033065f826bea
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-05-10 17:55:54 +0900

    refactoring

--- my/interpreters/jlox/src/com/craftinginterpreters/lox/Resolver.java
+++ my/interpreters/jlox/src/com/craftinginterpreters/lox/Resolver.java
@@ -21,15 +21,6 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         this.interpreter = interpreter;
     }
 
-    // Stack を使って，ブロック単位でスコープを管理する
-    @Override
-    public Void visitBlockStmt(Stmt.Block stmt) {
-        beginScope();
-        resolve(stmt.statements);
-        endScope();
-        return null;
-    }
-
     void resolve(List<Stmt> statements) {
         for (Stmt statement : statements) {
             resolve(statement);
@@ -44,12 +35,70 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         expr.accept(this);
     }
 
+    private void resolveLocal(Expr expr, Token name) {
+        // スコープの内から外側へ向かって変数を探索する．
+        for (int i = scopes.size() - 1; i >= 0; i--) {
+            if (scopes.get(i).containsKey(name.lexeme)) {
+                interpreter.resolve(
+                    expr,
+                    scopes.size() - 1 - i // 最も内側のスコープから，変数を発見するまでに巡ったスコープの数
+                );
+                return;
+            }
+        }
+    }
+
+    private void resolveFunction(Stmt.Function function, FunctionType type) {
+        FunctionType enclosingFunction = currentFunction;
+        currentFunction = type;
+
+        beginScope();
+        for (Token param : function.params) {
+            declare(param);
+            define(param);
+        }
+
+        // NOTE: 関数宣言のタイミングで，本文の解決も行う（関数呼び出し時ではない）．
+        resolve(function.body);
+        endScope();
+
+        currentFunction = enclosingFunction;
+    }
+
     private void beginScope() {
         scopes.push(new HashMap<String, Boolean>());
     }
 
     private void endScope() {
         scopes.pop();
+    }
+
+    private void declare(Token name) {
+        if (scopes.isEmpty()) return;
+
+        Map<String, Boolean> scope = scopes.peek();
+
+        if (scope.containsKey(name.lexeme)) {
+            Lox.error(name, "Already a variable with this name in this scope.");
+        }
+
+        scope.put(name.lexeme, false); // false: 宣言されたが初期化されていない
+    }
+
+    private void define(Token name) {
+        if (scopes.isEmpty()) return;
+        scopes.peek().put(name.lexeme, true); // true: 宣言されて初期化もされた
+    }
+
+    /* --- 文（Statement） --- */
+
+    // Stack を使って，ブロック単位でスコープを管理する
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        beginScope();
+        resolve(stmt.statements);
+        endScope();
+        return null;
     }
 
     @Override
@@ -77,53 +126,6 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         return null;
     }
 
-    private void declare(Token name) {
-        if (scopes.isEmpty()) return;
-
-        Map<String, Boolean> scope = scopes.peek();
-
-        if (scope.containsKey(name.lexeme)) {
-            Lox.error(name, "Already a variable with this name in this scope.");
-        }
-
-        scope.put(name.lexeme, false); // false: 宣言されたが初期化されていない
-    }
-
-    private void define(Token name) {
-        if (scopes.isEmpty()) return;
-        scopes.peek().put(name.lexeme, true); // true: 宣言されて初期化もされた
-    }
-
-    @Override
-    public Void visitVariableExpr(Expr.Variable expr) {
-        if (!scopes.isEmpty() && scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
-            Lox.error(expr.name, "Cannot read local variable in its own initializer.");
-        }
-
-        resolveLocal(expr, expr.name);
-        return null;
-    }
-
-    private void resolveLocal(Expr expr, Token name) {
-        // スコープの内から外側へ向かって変数を探索する．
-        for (int i = scopes.size() - 1; i >= 0; i--) {
-            if (scopes.get(i).containsKey(name.lexeme)) {
-                interpreter.resolve(
-                    expr,
-                    scopes.size() - 1 - i // 最も内側のスコープから，変数を発見するまでに巡ったスコープの数
-                );
-                return;
-            }
-        }
-    }
-
-    @Override
-    public Void visitAssignExpr(Expr.Assign expr) {
-        resolve(expr.value);
-        resolveLocal(expr, expr.name);
-        return null;
-    }
-
     @Override
     public Void visitFunctionStmt(Stmt.Function stmt) {
         declare(stmt.name);
@@ -131,23 +133,6 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         // NOTE: 関数本文から自身を呼び出せる（再帰を実現できる）ようにするため，事前に関数名を declare & define する．
         resolveFunction(stmt, FunctionType.FUNCTION);
         return null;
-    }
-
-    private void resolveFunction(Stmt.Function function, FunctionType type) {
-        FunctionType enclosingFunction = currentFunction;
-        currentFunction = type;
-
-        beginScope();
-        for (Token param : function.params) {
-            declare(param);
-            define(param);
-        }
-
-        // NOTE: 関数宣言のタイミングで，本文の解決も行う（関数呼び出し時ではない）．
-        resolve(function.body);
-        endScope();
-
-        currentFunction = enclosingFunction;
     }
 
     @Override
@@ -190,6 +175,25 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitWhileStmt(Stmt.While stmt) {
         resolve(stmt.condition);
         resolve(stmt.body);
+        return null;
+    }
+
+    /* --- 式（Expression） --- */
+
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr) {
+        if (!scopes.isEmpty() && scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+            Lox.error(expr.name, "Cannot read local variable in its own initializer.");
+        }
+
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
+    @Override
+    public Void visitAssignExpr(Expr.Assign expr) {
+        resolve(expr.value);
+        resolveLocal(expr, expr.name);
         return null;
     }
 
