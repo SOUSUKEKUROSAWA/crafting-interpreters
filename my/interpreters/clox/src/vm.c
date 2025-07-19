2a13119f8299cb0dc5eb3682ca62d0c1e646c70d
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-07-19 18:23:15 +0900

    15.1.1 命令を実行する

--- my/interpreters/clox/src/vm.c
+++ my/interpreters/clox/src/vm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -7,3 +9,44 @@ VM vm;
 void initVM() {}
 
 void freeVM() {}
+
+// NOTE: VM のコアとなる関数
+static InterpretResult run() {
+
+// 現在 ip が指している命令を読みだしてから，ip を１つ次に進める．
+#define READ_BYTE() (*vm.ip++)
+
+/**
+ * 現在 ip が指している命令を読みだし，
+ * その値をインデックスとして持つチャンクの定数プールから，
+ * 対応する値を取り出す．
+ * （ip を１つ次に進め）
+ */
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;) {
+        uint8_t instruction;
+
+        // 命令（バイトコード）のデコード（ディスパッチ）を行う．
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code; // チャンクの最初のバイトコード（命令）へのポインタを渡すことで初期化する．
+    return run();
+}
--- my/interpreters/clox/src/vm.h
+++ my/interpreters/clox/src/vm.h
@@ -5,9 +5,19 @@
 
 typedef struct {
     Chunk* chunk;
+
+    // NOTE: 高速化のためバイトコード配列内を直接参照する．
+    uint8_t* ip; // 次に実行する命令へのポインタ（命令ポインタ = Instruction Pointer の略）
 } VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
 
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk* chunk);
 
 #endif
