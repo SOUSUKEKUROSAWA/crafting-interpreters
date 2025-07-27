ca9459766ed2b8727380cb7eb64088857f1537db
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-07-27 20:38:57 +0900

    16.2 トークンを1個ずつ

--- my/interpreters/clox/src/compiler.c
+++ my/interpreters/clox/src/compiler.c
@@ -6,4 +6,29 @@
 
 void compile(const char* source) {
     initScanner(source);
+
+    int line = -1;
+    for (;;) {
+        // トークンの読み込み
+        Token token = scanToken();
+
+        // 行番号の表示
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf(" | ");
+        }
+
+        /**
+         * トークンの表示
+         *
+         * %.*s について
+         *  token.length で表示する文字数を指定し，
+         *  token.start で開始位置を指定している．
+         */
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
 }
