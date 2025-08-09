1f43006e1b3c2af4270f7ef8c02af93ae299acef
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-08-09 16:43:45 +0900

    18.4 2つの型を追加する

--- my/interpreters/clox/src/chunk.h
+++ my/interpreters/clox/src/chunk.h
@@ -10,6 +10,9 @@
  */
 typedef enum {
     OP_CONSTANT, // 特定の数を生成する．operand は生成する変数を表す．
+    OP_NIL, // nil をスタックにプッシュする．
+    OP_TRUE, // true をスタックにプッシュする．
+    OP_FALSE, // false をスタックにプッシュする．
     OP_ADD, // 加算
     OP_SUBSTRACT, // 減算
     OP_MULTIPLY, // 乗算
--- my/interpreters/clox/src/debug.c
+++ my/interpreters/clox/src/debug.c
@@ -45,6 +45,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     switch (instruction) {
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_NIL:
+            return simpleInstruction("OP_NIL", offset);
+        case OP_TRUE:
+            return simpleInstruction("OP_TRUE", offset);
+        case OP_FALSE:
+            return simpleInstruction("OP_FALSE", offset);
 
         /**
          * NOTE: 算術演算子（+ など）はスタックからオペランドを取得するが，
