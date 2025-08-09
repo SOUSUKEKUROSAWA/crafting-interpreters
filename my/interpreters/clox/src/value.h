c72bebcde9275b7f57e1184869ac38d949e7684e
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-08-09 12:56:08 +0900

    18.2 Lox の値と C の値

--- my/interpreters/clox/src/value.h
+++ my/interpreters/clox/src/value.h
@@ -23,6 +23,27 @@ typedef struct {
     } as;
 } Value;
 
+/**
+ * AS_* マクロが誤った型解釈をしないようにするための型判定．
+ *
+ * WARNING: 誤った型解釈の例
+ *      Value value = BOOL_VAL(true);
+ *      double number = AS_NUMBER(value);
+ */
+#define IS_BOOL(value)      ((value).type == VAL_BOOL)
+#define IS_NIL(value)       ((value).type == VAL_NIL)
+#define IS_NUMBER(value)    ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)      ((value).as.boolean) // Value 型をアンパックしてC言語の bool 型として値を取り出す．
+#define AS_NUMBER(value)    ((value).as.number) // Value 型をアンパックしてC言語の double 型として値を取り出す．
+
+/**
+ * NOTE: 静的な型を持つ値を，動的な型を持つ clox の値に昇格させている．
+ */
+#define BOOL_VAL(value)     ((Value){VAL_BOOL, {.boolean = value}}) // 渡されたC言語の値から，ブール型の Value を生成する．
+#define NIL_VAL(value)      ((Value){VAL_NIL, {.number = 0}}) // 渡されたC言語の値から，nil 型の Value を生成する．
+#define NUMBER_VAL(value)   ((Value){VAL_NUMBER, {.number = value}}) // 渡されたC言語の値から，数値型の Value を生成する．
+
 typedef struct {
     int capacity; // 総容量
     int count; // 要素数（利用済みの容量）
