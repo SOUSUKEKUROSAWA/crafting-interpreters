137c87dc44eb4319b2088116b00c5ce69e6dbe0b
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-10-05 14:35:24 +0900

    24.6 return 文

--- my/interpreters/clox/src/compiler.c
+++ my/interpreters/clox/src/compiler.c
@@ -194,6 +194,9 @@ static void emitLoop(int loopStart) {
     emitByte(offset & 0xff); // 下位 8 bit を出力
 }
 
+/**
+ * 戻り値なしの return，もしくは暗黙の return．
+ */
 static void emitReturn() {
     emitByte(OP_NIL); // NOTE: 暗黙の戻り値
     emitByte(OP_RETURN);
@@ -933,6 +936,20 @@ static void printStatement() {
     emitByte(OP_PRINT);
 }
 
+static void returnStatement() {
+    if (current->type == TYPE_SCRIPT) {
+        error("Can't return from top-level code.");
+    }
+
+    if (match(TOKEN_SEMICOLON)) {
+        emitReturn();
+    } else {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+        emitByte(OP_RETURN);
+    }
+}
+
 static void whileStatement() {
     int loopStart = currentChunk()->count;
 
@@ -997,6 +1014,8 @@ static void statement() {
         forStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
+    } else if (match(TOKEN_RETURN)) {
+        returnStatement();
     } else if (match(TOKEN_WHILE)) {
         whileStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
