d5e867b95480aac333492a629f38361899ae2e34
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-11-15 15:00:49 +0900

    28.1.2 メソッド宣言をコンパイルする

--- my/interpreters/clox/src/compiler.c
+++ my/interpreters/clox/src/compiler.c
@@ -696,6 +696,8 @@ static void string(bool canAssign) {
 }
 
 /**
+ * 変数のスコープ解決を行い，変数の読み取りまたは代入に対応するバイトコードを出力する．
+ *
  * @param canAssign true: 現在の優先度が，代入が許されるほど低い．
  *        e.g. a * b = c + d; の場合は false．
  */
@@ -876,15 +878,31 @@ static void function(FunctionType type) {
     }
 }
 
+static void method() {
+    consume(TOKEN_IDENTIFIER, "Expect method name.");
+    uint8_t constant = identifierConstant(&parser.previous);
+
+    FunctionType type = TYPE_FUNCTION;
+    function(type);
+
+    emitBytes(OP_METHOD, constant);
+}
+
 static void classDeclaration() {
     consume(TOKEN_IDENTIFIER, "Expect class name.");
+    Token className = parser.previous;
     uint8_t nameConstant = identifierConstant(&parser.previous);
     declareVariable();
     emitBytes(OP_CLASS, nameConstant);
     defineVariable(nameConstant); // クラス本文よりも前で定義済みにすることで，自己参照（self）などが実現可能になる．
 
+    namedVariable(className, false); // クラスがスタックにロードされる．
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
+    while (!check(TOKEN_RIGHT_BRACE && !check(TOKEN_EOF))) {
+        method();
+    }
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
+    emitByte(OP_POP);
 }
 
 static void funDeclaration() {
