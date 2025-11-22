1134e5120c4ab9f6f3d15e218ebe452f9e76c0da
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-11-22 15:30:58 +0900

    デバッグモードをオフにする

--- my/interpreters/clox/src/common.h
+++ my/interpreters/clox/src/common.h
@@ -12,7 +12,7 @@
 
 // #define DEBUG_PRINT_CODE
 
-#define DEBUG_TRACE_EXECUTION // オペコードやスタックの値を出力するモード．@warning 性能には悪影響を及ぼす．
+// #define DEBUG_TRACE_EXECUTION // オペコードやスタックの値を出力するモード．@warning 性能には悪影響を及ぼす．
 
 // #define DEBUG_STRESS_GC // GCをメモリの追加割り当てを行う度に実行し，メモリ管理のバグを見つけやすくする，ストレステストモード．@warning 性能には悪影響を及ぼす．
 // #define DEBUG_LOG_GC // 動的メモリで何か行う度に，その情報を出力するロギングモード．@warning 性能には悪影響を及ぼす．
