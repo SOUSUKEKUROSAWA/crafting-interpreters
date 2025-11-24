ebe878f4acdd85a99c5117e4491af0f0640043be
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2025-11-24 13:29:33 +0900

    30.3.6 Value 関数

--- my/interpreters/clox/src/value.c
+++ my/interpreters/clox/src/value.c
@@ -35,15 +35,34 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+    if (IS_BOOL(value)) {
+        printf(AS_BOOL(value) ? "true" : "false");
+    } else if (IS_NIL(value)) {
+        printf("nil");
+    } else if (IS_NUMBER(value)) {
+        printf("%g", AS_NUMBER(value));
+    } else if (IS_OBJ(value)) {
+        printObject(value);
+    }
+#else
     switch (value.type) {
         case VAL_BOOL: printf(AS_BOOL(value) ? "true" : "false"); break;
         case VAL_NIL: printf("nil"); break;
         case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
         case VAL_OBJ: printObject(value); break;
     }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+    if (IS_NUMBER(a) && IS_NUMBER(b)) {
+        // @warning: NaN == NaN だけはビット表現が一致していても false として扱う必要がある．
+        return AS_NUMBER(a) == AS_NUMBER(b);
+    }
+    return a == b; // NaN同士以外は，単純にビット表現を比較するだけでOK．
+#else
     if (a.type != b.type) return false;
 
     switch (a.type) {
@@ -53,4 +72,5 @@ bool valuesEqual(Value a, Value b) {
         case VAL_OBJ: return AS_OBJ(a) == AS_OBJ(b); // NOTE: 文字列はインターン化されているので，ポインタが等しければ必ず等しい．
         default: return false;
     }
+#endif
 }
